56a467bececcd0c208a552bc51bc96de09279d49
Liam Walsh <liw6800@gmail.com>
2020-11-19 17:02:10 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -27,7 +27,8 @@ def won?(board)
     if position_1 == "X" && position_2 == "X" && position_3 == "X" || position_1 == "O" && position_2 == "O" && position_3 == "O"
       return combo
     end
-  return false
+  end
+ else return false
 end
 
 def full?(board)
