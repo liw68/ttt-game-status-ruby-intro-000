8fed8e6eaaec65187a43e81909d4dde08d1d2fc0
Liam Walsh <liw6800@gmail.com>
2020-11-19 17:06:23 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -20,9 +20,9 @@ def won?(board)
    index_2 = combo[1]
    index_3 = combo[2]
 
-    board[index_1] = position_1
-    board[index_2] = position_2
-    board[index_3] = position_3
+   board[index_1] = position_1
+   board[index_2] = position_2
+   board[index_3] = position_3
 
     if position_1 == "X" && position_2 == "X" && position_3 == "X" || position_1 == "O" && position_2 == "O" && position_3 == "O"
       return combo
