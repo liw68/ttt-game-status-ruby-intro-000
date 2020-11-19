68564c3103d3d74084b8041ceed16302ac037869
Liam Walsh <liw6800@gmail.com>
2020-11-19 16:39:26 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -3,4 +3,43 @@ def position_taken?(board, index)
   !(board[index].nil? || board[index] == " ")
 end
 
-# Define your WIN_COMBINATIONS constant
+WIN_COMBINATIONS = [
+  [0,1,2],
+  [3,4,5],
+  [6,7,8],
+  [0,3,6],
+  [1,4,7],
+  [2,5,8],
+  [0,4,8],
+  [2,4,6]
+]
+
+def won?(board)
+  WIN_COMBINATIONS.each do |combo|
+    combo.all? do |position_filled|
+      position_filled == "X"
+        return combo
+      end
+    combo.all? do |position_filled|
+      position_filled =="O"
+            return combo
+          end
+    end
+  return false
+
+end
+
+def full?(board)
+  board.all? do |index|
+    index == "X" || index == "O"
+  end
+end
+
+def draw?(board)
+end
+
+def over?(board)
+end
+
+def winner?(board)
+end
